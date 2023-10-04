51bac0621b5f7c78b62d98cf380dbb7c6f807a19
DSulzhits <dsulzhits@gmail.com>
2023-10-04 15:55:48 +0300

    add command create database (ccdb) (добавлена команда для создания базы данных (ccdb))

---
+++ suppliers/management/commands/ccdb.py
@@ -0,0 +1,26 @@
+from django.core.management import BaseCommand
+import psycopg2
+import psycopg2.errors
+from config.settings import database_name, database_password, database_host, database_port, database_user
+
+
+class Command(BaseCommand):
+    """Add command to create database
+    (Добавлена команда для создания базы данных)"""
+
+    def handle(self, *args, **options):
+        conn = psycopg2.connect(
+            database='postgres',
+            user=database_user,
+            password=database_password,
+            host=database_host,
+            port=database_port
+        )
+        conn.autocommit = True
+        cur = conn.cursor()
+
+        try:
+            cur.execute(f"CREATE DATABASE {database_name}")
+        except psycopg2.errors.DuplicateDatabase:
+            print(f'ОШИБКА: база данных {database_name} уже существует')
+        conn.close()
