4d4187ba7a36cd5e6819cc5a635768601c0fd732
DSulzhits <dsulzhits@gmail.com>
2023-10-06 15:43:10 +0300

    register model User to admin (модель User зарегистрирована в Админке)

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from users.models import User
 
-# Register your models here.
+
+@admin.register(User)
+class ElectronicsTradeAdmin(admin.ModelAdmin):
+    """Register User model in Admin
+    (Модель User зарегистрирована в админке"""
+    list_display = ('id', 'email', 'is_active',)
+    list_filter = ('id', 'email',)
+    ordering = ('id',)
