fc06eb88e9462d42ec3505c383b7d8f6a1644a1d
DSulzhits <dsulzhits@gmail.com>
2023-10-06 14:08:46 +0300

    create views for model ChainElement (добавлены контроллеры для модели ChainElement)

--- suppliers/views.py
+++ suppliers/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
+from rest_framework.permissions import IsAuthenticated
+from suppliers.serializers.chainelement_serializres import ChainElementSerializer, ChainElementListSerializer, \
+    ChainElementCreateSerializer, ChainElementUpdateSerializer
+from users.permissions import IsActive, IsSuperuser, IsModerator
 
-# Create your views here.
+from suppliers.models import ChainElement
+
+
+class ChainElementListAPIView(ListAPIView):
+    serializer_class = ChainElementListSerializer
+    queryset = ChainElement.objects.all()
+    permission_classes = [IsAuthenticated, IsActive]
+
+
+class ChainElementCreateAPIView(CreateAPIView):
+    serializer_class = ChainElementCreateSerializer
+    queryset = ChainElement.objects.all()
+    permission_classes = [IsAuthenticated, IsActive]
+
+
+class ChainElementRetrieveAPIView(RetrieveAPIView):
+    serializer_class = ChainElementSerializer
+    queryset = ChainElement.objects.all()
+    permission_classes = [IsAuthenticated, IsActive]
+
+
+class ChainElementUpdateAPIView(UpdateAPIView):
+    serializer_class = ChainElementUpdateSerializer
+    queryset = ChainElement.objects.all()
+    permission_classes = [IsAuthenticated, IsActive]
+
+
+class ChainElementDestroyAPIView(DestroyAPIView):
+    serializer_class = ChainElementSerializer
+    queryset = ChainElement.objects.all()
+    permission_classes = [IsAuthenticated, IsActive, IsModerator | IsSuperuser]
