9e000e4ec5cc461497133806b9982ae2af9f392c
DSulzhits <dsulzhits@gmail.com>
2023-10-09 15:34:05 +0300

    Add new admin action clear_summ (добавлено новое действие в админке clear_summ)

--- suppliers/admin.py
+++ suppliers/admin.py
@@ -2,10 +2,16 @@ from django.contrib import admin
 from suppliers.models import ChainElement
 
 
+@admin.action(description="Clear debt summ")
+def clear_summ(modeladmin, request, queryset):
+    queryset.update(debt=0)
+
+
 @admin.register(ChainElement)
 class ChainElementAdmin(admin.ModelAdmin):
     """Register ChainElement model in Admin
     (Модель ChainElement зарегистрирована в админке"""
-    list_display = ('id', 'supplier', 'name',)
+    list_display = ('id', 'supplier', 'name', 'debt',)
     list_filter = ('id', 'city',)
     ordering = ('id',)
+    actions = [clear_summ]
