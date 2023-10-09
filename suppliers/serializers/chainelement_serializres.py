8e5e094afc01940e6deb42a70072a5382109235f
DSulzhits <dsulzhits@gmail.com>
2023-10-09 16:39:12 +0300

    Update serializers (Обновлены сериализаторы)

--- suppliers/serializers/chainelement_serializres.py
+++ suppliers/serializers/chainelement_serializres.py
@@ -1,26 +1,35 @@
 from rest_framework.serializers import ModelSerializer
 from suppliers.models import ChainElement
+from rest_framework.relations import SlugRelatedField
 
 
 class ChainElementSerializer(ModelSerializer):
+    supplier = SlugRelatedField(slug_field="name", queryset=ChainElement.objects.all())
+
     class Meta:
         model = ChainElement
         fields = '__all__'
 
 
 class ChainElementListSerializer(ModelSerializer):
+    supplier = SlugRelatedField(slug_field="name", queryset=ChainElement.objects.all())
+
     class Meta:
         model = ChainElement
-        fields = ('name', 'email', 'supplier')
+        fields = ('id', 'name', 'email', 'supplier')
 
 
 class ChainElementCreateSerializer(ModelSerializer):
+    supplier = SlugRelatedField(slug_field="name", queryset=ChainElement.objects.all())
+
     class Meta:
         model = ChainElement
         exclude = ('creation_time',)
 
 
 class ChainElementUpdateSerializer(ModelSerializer):
+    debt = ChainElementSerializer(many=False, read_only=True, source='debt.all')
+
     class Meta:
         model = ChainElement
         exclude = ('id', 'creation_time',)
