1fd06e643f9e71ca0b95503b22070831d650c3a0
DSulzhits <dsulzhits@gmail.com>
2023-10-18 14:20:33 +0300

    Add Product serializers (Добавлены сериализаторы Product)

---
+++ suppliers/serializers/product_serializers.py
@@ -0,0 +1,35 @@
+from rest_framework.serializers import ModelSerializer
+from suppliers.models import ChainElement, Product
+from rest_framework.relations import SlugRelatedField
+
+
+class ProductSerializer(ModelSerializer):
+    seller = SlugRelatedField(slug_field="name", queryset=ChainElement.objects.all())
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+
+class ProductListSerializer(ModelSerializer):
+    seller = SlugRelatedField(slug_field="name", queryset=ChainElement.objects.all())
+
+    class Meta:
+        model = Product
+        fields = ('id', 'name', 'seller',)
+
+
+class ProductCreateSerializer(ModelSerializer):
+    seller = SlugRelatedField(slug_field="name", queryset=ChainElement.objects.all())
+
+    class Meta:
+        model = Product
+        exclude = ('created_at',)
+
+
+class ProductUpdateSerializer(ModelSerializer):
+    seller = SlugRelatedField(slug_field="name", queryset=ChainElement.objects.all())
+
+    class Meta:
+        model = Product
+        exclude = ('id', 'created_at',)
