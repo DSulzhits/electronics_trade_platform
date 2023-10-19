00359469c51b3061e7f50d6cf49831af0def4dc9
DSulzhits <dsulzhits@gmail.com>
2023-10-19 17:45:41 +0300

    Add Product urlpatterns (Добавлены пути для контроллеров Product)

--- suppliers/urls.py
+++ suppliers/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter
 from suppliers.apps import SuppliersConfig
 from suppliers.views import ChainElementListAPIView, ChainElementCreateAPIView, ChainElementRetrieveAPIView, \
-    ChainElementUpdateAPIView, ChainElementDestroyAPIView
+    ChainElementUpdateAPIView, ChainElementDestroyAPIView, ProductListAPIView, ProductCreateAPIView, \
+    ProductRetrieveAPIView, ProductUpdateAPIView, ProductDestroyAPIView
 
 app_name = SuppliersConfig.name
 
@@ -15,4 +16,11 @@ urlpatterns = [
     path('chain_element/<int:pk>/', ChainElementRetrieveAPIView.as_view(), name='chain_element_detail'),
     path('chain_element/<int:pk>/update/', ChainElementUpdateAPIView.as_view(), name='chain_element_update'),
     path('chain_element/<int:pk>/delete', ChainElementDestroyAPIView.as_view(), name='chain_element_delete'),
+
+    # Product urlpatterns
+    path('products/', ProductListAPIView.as_view(), name='products_list'),
+    path('product/create/', ProductCreateAPIView.as_view(), name='product_create'),
+    path('product/<int:pk>/', ProductRetrieveAPIView.as_view(), name='product_detail'),
+    path('product/<int:pk>/update/', ProductUpdateAPIView.as_view(), name='product_update'),
+    path('product/<int:pk>/delete', ProductDestroyAPIView.as_view(), name='product_delete'),
 ] + router.urls
